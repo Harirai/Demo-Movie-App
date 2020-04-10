6ab19ff56c7b579d2e984c1ca421b25a2905ebda
Harirai <harim1709@gmail.com>
2020-04-11 04:27:43 +0530

    added name to views for reverse look ups

--- movieapp/movie/urls.py
+++ movieapp/movie/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('movies/', views.MovieList.as_view()),
-    path('movies/<int:pk>', views.MovieDetail.as_view()),
-    path('users/', views.UserList.as_view()),  # new
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('movies/', views.MovieList.as_view(),  name='movie-list'),
+    path('movies/<int:pk>', views.MovieDetail.as_view(),  name='movie-detail'),
+    path('users/', views.UserList.as_view(),  name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(),  name='user-detail'),
     path('', views.api_root),
 ]
 
